01ee093bdb5182593b2d8dc9626ac97f40134e80
dkakhniashvili <dkakhniashvili@DKAKHNIASHVILI>
2024-09-13 12:06:40 +0400

    Add project files.

---
+++ Chess_Figures/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+Console.WriteLine("sheiyvane ra ginda ro gaormagdes");
+
+
+Console.WriteLine(kvadrati(Convert.ToInt32(Console.ReadLine())));
+
+
+static int kvadrati(int num)
+{
+    int result = num * num;
+    return result;
+}
